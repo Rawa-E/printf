1b4c0db4a303ecd99c2ce9858608bc2e312bf781
Maxst1270 <alisuna225@gmail.com>
2023-10-17 18:23:53 -0700

    a

---
+++ check_f.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *check_f - checks the string for specs
+ *@format:the string form
+ *@pr:arguments
+*/
+void check_f(const char *format, va_list pr)
+{
+	for (; *format != '\0'; format++)
+	{
+		if (*format == '%')
+		{
+			format++;
+			_handler(*format, pr);
+		}
+		else
+			_putchar(*format);
+	}
+}
